0705bf5ab407976dee51334fc71949dd5030d899
zaya-a <rekaprekopcsak@gmail.com>
2021-06-03 15:05:24 +0200

    navbar felig meddig

---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+window.addEventListener("load",init, false)
+let szamlalo = 0;
+function $(id){
+    return document.getElementById(id);
+}
+function init(){
+    $("mobile-menu").addEventListener("click",menu,false);
+}
+
+function menu(){
+    szamlalo++;
+    if(szamlalo % 2 ==0){
+        megjelen();
+    }else{
+        eltun();
+    }
+}
+
+function megjelen(){
+    for(let i = 0; i<5;i++){
+        $(i+".menu").style.display ="block";
+    }
+}
+
+function eltun(){
+    for(let i = 0; i<5;i++){
+        $(i+".menu").style.display ="none";
+    }
+}
